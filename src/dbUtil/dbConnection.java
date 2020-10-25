2b80acd2bc2071caf6b5be064a144d9f9edc9aff
g-ivanova <72075589+g-ivanova@users.noreply.github.com>
2020-10-25 16:02:54 +0200

    Update dbConnection.java

--- src/dbUtil/dbConnection.java
+++ src/dbUtil/dbConnection.java
@@ -1,1 +1,30 @@
 
+package dbUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class dbConnection {
+
+    Connection c=null;
+    Statement stmt = null;
+    private static final String SQCONN = "jdbc:sqlite:HotelSystem.sqlite";
+
+    public static dbConnection con;
+    public static Connection getConnection() throws SQLException
+    {
+
+                try{
+                    Class.forName("org.sqlite.JDBC");
+                    return DriverManager.getConnection("jdbc:sqlite:HotelSystem.db");
+
+                }
+                catch(ClassNotFoundException ex){
+                    ex.printStackTrace();
+                }
+                return null;
+    }
+
+}
