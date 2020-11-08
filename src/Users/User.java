173f756aa729d723f9df2e2d376201c2f7fde866
g-ivanova <72075589+g-ivanova@users.noreply.github.com>
2020-11-08 22:19:54 +0200

    Create User.java

---
+++ src/Users/User.java
@@ -0,0 +1,14 @@
+package Users;
+
+public class User {
+    public int id_user;
+
+    public User(){}
+
+    public User(int id_user){
+        this.id_user=id_user;
+    }
+    public int getId_user(){
+        return this.id_user;
+    }
+}
